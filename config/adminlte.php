7ce0ec67ea7c0feeb189ea4c72c5aba53c7ee69d
Ander <anderson.blandon@hotmail.com>
2025-05-02 10:37:41 -0500

    Agregué referencias formateo COP, se organizo el logo y el nombre de la empresa

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => 'Sistema de prestamos y cobranzas',
+    'title' => 'NEXUS INVERSIONES',
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -63,7 +63,7 @@ return [
     |
     */
 
-    'logo' => '<b>SIS</b> PRESTAMOS',
+    'logo' => '<b>NEXUS</b> INVERSIONES',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
